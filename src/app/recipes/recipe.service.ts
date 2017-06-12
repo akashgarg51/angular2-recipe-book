24c8bc5b583bd602335f93d4b2da78c10b293714
Akash Garg <akash.garg@sap.com>
2017-06-12 19:23:52 +0530

    Cross service communication to add shopping items from recipe
    Change-Id: I729d78dd2cc8a2679ecee4dd4e48287213e399c4
    Signed-off-by: Akash Garg <akash.garg@sap.com>

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
-
 import { Recipe } from "./recipe";
+import { Ingredient } from "../shared/ingredient";
+
 @Injectable()
 export class RecipeService {
    private recipes: Recipe[] = [
-     new Recipe('Cake', 'Very Tasty', 'https://s-media-cache-ak0.pinimg.com/736x/29/ee/96/29ee96df49e02fae0ecd190bff81adbb.jpg', []),
+     new Recipe('Cake', 'Very Tasty', 'https://s-media-cache-ak0.pinimg.com/736x/29/ee/96/29ee96df49e02fae0ecd190bff81adbb.jpg', [
+       new Ingredient('Chocolate', 1),
+       new Ingredient('Cherry', 2)   
+           ]),
      new Recipe('Salad', 'Good', 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2009/6/17/3/FNM080109Insert030_s4x3.jpg.rend.hgtvcom.1280.960.jpeg', [])
       ];
   constructor() { }
