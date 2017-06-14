8025f63d34a6df020bef80d1df930fe483d42b5c
Akash Garg <akash.garg@sap.com>
2017-06-14 14:34:26 +0530

    Passing RecipeId from RecipeItem to detail page
    Change-Id: Ic79c8a3824c31195dbeda4aa3dc9eb0e9ca60181
    Signed-off-by: Akash Garg <akash.garg@sap.com>

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'rb-recipes',
   templateUrl: './recipes.component.html'
 })
-export class RecipesComponent implements OnInit {
+export class RecipesComponent{
 //  selectedRecipe: Recipe;    (don't need with routing concept)
 
 }
