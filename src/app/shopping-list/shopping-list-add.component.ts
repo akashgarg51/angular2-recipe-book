ff4b7fbfc42f812980baa2062cdd6d6e6aec2296
Akash Garg <akash.garg@sap.com>
2017-06-18 11:57:57 +0530

    Type correction
    Change-Id: I2a4ab1d1f83635b531360d6b45dd1e1149651558
    Signed-off-by: Akash Garg <akash.garg@sap.com>

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 import { Ingredient } from "../shared/ingredient";
 import { ShoppingListService } from "./shopping-list.service"
 
