8152fbeb1b6d3258777e8d9896849d4d10623095
Akash Garg <akash.garg@sap.com>
2017-06-18 17:48:30 +0530

    Completing Angular2 forms
    Change-Id: Ifaa7ce32223b6c7802a9e204863f1f15768c8d72
    Signed-off-by: Akash Garg <akash.garg@sap.com>

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { RecipeService } from "../recipe.service";
 
 import { Subscription } from "rxjs/Rx";
@@ -12,15 +12,16 @@ import { FormArray, FormGroup, FormControl, Validators, FormBuilder } from "@ang
     styles: []
 })
 export class RecipeEditComponent implements OnInit, OnDestroy {
-    recipeForm: FormGroup; 
+    recipeForm: FormGroup;
     private recipeIndex: number;
     private recipe: Recipe;
     private isNew = true;
     private subscription: Subscription;
 
-    constructor(private route: ActivatedRoute, 
-    private recipeService: RecipeService,
-    private formBuilder: FormBuilder) { }
+    constructor(private route: ActivatedRoute,
+        private recipeService: RecipeService,
+        private formBuilder: FormBuilder,
+        private router: Router) { }
 
     ngOnInit() {
         this.subscription = this.route.params.subscribe(
@@ -37,33 +38,67 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
             }
         );
     }
-    
+
     ngOnDestroy() {
-         this.subscription.unsubscribe();
+        this.subscription.unsubscribe();
     }
-    
+
+    private navigateBack() {
+        this.router.navigate(['../']);
+    }
+
+    onSubmit() {
+        const newRecipe = this.recipeForm.value;
+        if (this.isNew) {
+            this.recipeService.addRecipe(newRecipe);
+        } else {
+            this.recipeService.editRecipe(this.recipe, newRecipe);
+        }
+        this.navigateBack();
+    }
+
+    onCancel() {
+        this.navigateBack();
+    }
+
+    onAddItem(name: string, amount: string) {
+        (<FormArray>this.recipeForm.controls['ingredients']).push(
+            new FormGroup({
+                name: new FormControl(name, Validators.required),
+                amount: new FormControl(amount, [
+                    Validators.required,
+                    Validators.pattern("\\d+") //it means it should be digit, means only numbers are allowed
+                ])
+            })
+        )
+    }
+
+    onRemoveItem(index: number) {
+        (<FormArray>this.recipeForm.controls['ingredients']).removeAt(index);
+    }
+
     private initForm() {
         let recipeName = '';
-        let recipeImageUrl = ''; 
+        let recipeImageUrl = '';
         let recipeContent = '';
         let recipeIngredients: FormArray = new FormArray([]);
-        
-        if(!this.isNew){
-            for (let i=0; i < this.recipe.ingredients.length; i++){
+
+        if (!this.isNew) {
+            for (let i = 0; i < this.recipe.ingredients.length; i++) {
                 recipeIngredients.push(
-                new FormGroup({
-                       name: new FormControl(this.recipe.ingredients[i].name, Validators.required),
-                       amount: new FormControl(this.recipe.ingredients[i].amount, [
-                       Validators.required, 
-                       Validators.pattern("\\d+") //it means it should be digit, means only numbers are allowed
-                       ])
+                    new FormGroup({
+                        name: new FormControl(this.recipe.ingredients[i].name, Validators.required),
+                        amount: new FormControl(this.recipe.ingredients[i].amount, [
+                            Validators.required,
+                            Validators.pattern("\\d+") //it means it should be digit, means only numbers are allowed
+                        ])
                     }));
+                recipeName = this.recipe.name;
+                recipeImageUrl = this.recipe.imagePath;
+                recipeContent = this.recipe.description;
             }
         }
-        recipeName = this.recipe.name;
-        recipeImageUrl = this.recipe.imagePath;
-        recipeContent = this.recipe.description;
-        
+
         this.recipeForm = this.formBuilder.group({
             name: [recipeName, Validators.required],
             imagePath: [recipeImageUrl, Validators.required],
