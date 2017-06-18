c0a6df0615ed202278895cd14775fdc1e8661800
Akash Garg <akash.garg@sap.com>
2017-06-18 11:42:17 +0530

    Addition of items into shopping list
    Change-Id: I8721591c6571df1d477328f34fe64f0994746e42
    Signed-off-by: Akash Garg <akash.garg@sap.com>

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from "../shared/ingredient";
+import { ShoppingListService } from "./shopping-list.service"
 
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
 export class ShoppingListAddComponent implements OnInit {
+  item: Ingredient;
   isAdd = true;
     
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
   }
-
+  
+  onSubmit(ingredient: Ingredient){  //As this form has name and amount so it would be of type Ingredient
+      if(!this.isAdd){
+          
+      } else{
+         this.item = new Ingredient(ingredient.name, ingredient.amount)
+         this.sls.addItem(this.item);
+      }
+  }
 }
